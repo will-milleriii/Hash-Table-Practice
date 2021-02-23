5dc9c2d7b2aa8e2b99d7f607dbf8bbe6552ead9a
Will Miller <willmiller@Wills-Air.fios-router.home>
2021-02-23 13:55:27 -0500

    hash table constructor finished

--- src/main/java/HashTable.java
+++ src/main/java/HashTable.java
@@ -1,2 +1,38 @@
 public class HashTable {
+
+    String[] hashArray;
+    int arraySize;
+    int size = 0; //counts number of elements in hash table
+
+    public HashTable(int numOfSlots) {
+
+        if (isPrime(numOfSlots)) {
+            hashArray = new String[numOfSlots];
+            arraySize = numOfSlots;
+        } else {
+            int primeCount = getNextPrime(numOfSlots);
+            hashArray = new String[primeCount];
+            arraySize = primeCount;
+
+            System.out.println("Hash Table size given " + numOfSlots + " is not a prime");
+            System.out.println("Hash Table size changed to: " + primeCount);
+        }
+    }
+
+    public boolean isPrime(int num){
+        for (int i = 2; i * i <= num; i++){
+            if (num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int getNextPrime(int minNum){
+        for (int i = minNum; true; i++){
+            if (isPrime(i)){
+                return i;
+            }
+        }
+    }
 }
